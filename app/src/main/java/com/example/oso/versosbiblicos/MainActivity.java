20fdb7eb2fce6587613f731aa8985c8eacddd35c
oso <eldetulado@gmail.com>
2017-11-15 18:29:00 -0400

    Conexion a Firebase

--- app/src/main/java/com/example/oso/versosbiblicos/MainActivity.java
+++ app/src/main/java/com/example/oso/versosbiblicos/MainActivity.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,7 +17,8 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     List<Message> list;
     RecyclerView rv;
-
+    MyAdapter adapter;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +26,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         rv = findViewById(R.id.lista);
-
         rv.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
 
         list = new ArrayList<>();
 
+        reference = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference productoRef = reference.child("productos");
 
-        for (int i = 0; i < 10; i++){
-            Message m = new Message(getString(R.string.verso), getString(R.string.info_verso));
-            list.add(m);
-        }
-
-        MyAdapter adapter = new MyAdapter(list);
-
+        adapter = new MyAdapter(list);
         rv.setAdapter(adapter);
+
+        productoRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot msgSnapshot : dataSnapshot.getChildren()){
+                    Message m = msgSnapshot.getValue(Message.class);
+                    list.add(m);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 }
